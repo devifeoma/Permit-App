5914b069de0bd2204f6282adb1da7045d715e7c2
devifeoma <judithcynthia1@gmail.com>
2022-04-08 14:13:23 +0100

    Deleted saga.js file

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import MainBody from "./components/MainBody";
+import Footer from "./components/Footer";
+import ViewDetails from "./components/ViewDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainBody />} />
+        <Route path="/viewDetails/:id" element={<ViewDetails />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Permit = {
+  id: string;
+  permit_: string;
+  permit_type: string;
+  review_type: string;
+  application_start_date: string;
+  issue_date: string;
+  processing_time: string;
+  street_number: string;
+  street_direction: string;
+  street_name: string;
+};
